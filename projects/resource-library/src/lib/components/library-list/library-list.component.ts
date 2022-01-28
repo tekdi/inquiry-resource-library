cb6e0724ce23c9aa70b5b6f905d8f73cb586d4ff
Dhruv Arya <dhruvarya9654@gmail.com>
2022-01-28 11:10:10 +0530

    changes for adding multiple contents

--- projects/resource-library/src/lib/components/library-list/library-list.component.ts
+++ projects/resource-library/src/lib/components/library-list/library-list.component.ts
@@ -12,9 +12,10 @@ import {EditorTelemetryService} from '../../services/telemetry/telemetry.service
 export class LibraryListComponent implements OnInit {
     @Input() contentList;
     @Input() showAddedContent: any;
-    @Output() contentChangeEvent = new EventEmitter<any>();
+    @Output() onChangeEvent = new EventEmitter<any>();
     @Output() moveEvent = new EventEmitter<any>();
     @Input() selectedContent: any;
+    public selectedContentList: any[] = [];
     public sortContent = false;
 
     constructor(public editorService: EditorService, public telemetryService: EditorTelemetryService,
@@ -25,7 +26,7 @@ export class LibraryListComponent implements OnInit {
     }
 
     onContentChange(selectedContent: any) {
-        this.contentChangeEvent.emit({content: selectedContent});
+        this.onChangeEvent.emit({type: 'content', content: selectedContent});
     }
 
     changeFilter() {
@@ -49,10 +50,20 @@ export class LibraryListComponent implements OnInit {
         });
     }
 
-    addToLibrary() {
-        this.moveEvent.emit({
-            action: 'addContent'
-        });
+    selectContent(content) {
+      const contentId = content.identifier
+      const index = this.selectedContentList.indexOf(contentId);
+      if (index === -1) {
+        this.selectedContentList.push(contentId);
+      }
+      else {
+        this.selectedContentList.splice(index, 1);
+      }
+      this.onChangeEvent.emit({
+        type: 'contentList',
+        contentList: this.selectedContentList,
+        contentType: content.objectType
+      });
     }
 
 }
