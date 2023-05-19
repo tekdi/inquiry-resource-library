00e201e28a7185f7ae238b377ac208a9667d8335
Harish Kumar Gangula <harish@sanketika.in>
2023-05-19 13:53:47 +0530

    Issue #KN-882 feat: Updated the players to web components

--- projects/resource-library/src/lib/resource-library.module.ts
+++ projects/resource-library/src/lib/resource-library.module.ts
@@ -13,8 +13,6 @@ import {LibraryFilterComponent} from './components/library-filter/library-filter
 import {LibraryListComponent} from './components/library-list/library-list.component';
 import {LibraryPlayerComponent} from './components/library-player/library-player.component';
 import {SkeletonLoaderComponent} from './components/skeleton-loader/skeleton-loader.component';
-import {SunbirdPdfPlayerModule} from '@project-sunbird/sunbird-pdf-player-v9';
-import {SunbirdVideoPlayerModule} from '@project-sunbird/sunbird-video-player-v9';
 import {QumlLibraryModule} from '@project-sunbird/sunbird-quml-player';
 import {CarouselModule} from 'ngx-bootstrap/carousel';
 import {TelemetryInteractDirective} from './directives/telemetry-interact/telemetry-interact.directive';
@@ -29,7 +27,7 @@ import { InterpolatePipe } from './pipes/interpolate/interpolate.pipe';
     ],
     imports: [
         CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild([]), SuiModule,
-        CommonFormElementsModule, InfiniteScrollModule, HttpClientModule, SunbirdPdfPlayerModule, SunbirdVideoPlayerModule,
+        CommonFormElementsModule, InfiniteScrollModule, HttpClientModule,
         QumlLibraryModule, CarouselModule
     ],
     providers: [
